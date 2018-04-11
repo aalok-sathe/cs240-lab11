641c82d1cc15cacbbd6c870000933409482c14a8
aalok.sathe <aalok.sathe@gmail.com>
2018-04-11 14:21:29 -0400

    populate initial tests	modified:   tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -3,7 +3,26 @@
 
 int main()
 {	
+    // Constructor
 	LinkedList<int> l;
-
+	l.add(3);
+	l.add(1);
+	l.add(42);
+	
+	// Size method test
+	std::cout << "l.size() = " << l.size() << "[3]" << std::endl;
+	
+	
+	
+	
+	// Copy constructor
+	LinkedList<int> copyList(l);
+	std::cout << "copyList.size() = " << copyList.size() << "[3]" << std::endl;
+	
+		
+	std::cout << l.
+	
+	// 
+	
 	return 0;
 }
