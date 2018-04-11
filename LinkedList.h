cf1d8028416f57e5dd3db728f853fea532b084da
aalok.sathe <aalok.sathe@gmail.com>
2018-04-11 13:51:40 -0400

    Initial commit 	new file:   LinkedList.h 	new file:   tester.cpp

---
+++ LinkedList.h
@@ -0,0 +1,42 @@
+#ifndef __LINKEDLIST_H__
+#define __LINKEDLIST_H__
+
+#include <list>
+#include <vector>
+
+template <class T>
+class LinkedList
+{
+   private:
+     std::list<T> theList;
+
+   public:
+     LinkedList();
+     LinkedList(const LinkedList<T>& other);
+     ~LinkedList();
+
+     int  size() const;
+     void add(T element);
+
+     T get(int index) const;
+     T remove(int index); 
+
+     std::vector<T> toArray() const;
+
+     LinkedList<T>& operator+=( const T& item );
+};
+
+template <class T> LinkedList<T>::LinkedList() {}
+
+template <class T> LinkedList<T>::~LinkedList() {}
+
+template <class T>
+LinkedList<T>::LinkedList(const LinkedList<T>& other)
+{
+    typename std::list<T>::iterator it = other.theList.begin();
+    while (it != other.theList.end())
+        this->theList.push_back(*it++);
+}
+
+
+#endif
