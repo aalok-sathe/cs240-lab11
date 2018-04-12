ed57055b4e575e48d4408051700984b4cfb9501d
aalok.sathe <aalok.sathe@gmail.com>
2018-04-12 01:35:21 -0400

    finish up	modified:   tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -16,7 +16,6 @@ int main()
 	std::cout << std::setw(20) << std::right << "Melissa Gu," << std::endl
 	          << std::setw(20) << std::right << "Aalok Sathe" << std::endl;
 
-
 	// Constructor
 	std::cout << std::endl
 	          << "-----------------------------------------------------" << std::endl;
